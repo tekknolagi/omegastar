094125c7b20549c655bc6aa67c3b3a4db7589c9b
Max Bernstein <max@bernsteinbear.com>
2024-09-10 01:19:13 -0400

    Add a negative number to make splitting harder

--- omegastar_test.py
+++ omegastar_test.py
@@ -42,12 +42,13 @@ class BisectTests(unittest.TestCase):
         def command(items):
             nonlocal n_steps
             n_steps += 1
-            return not (1 in items and 3 in items and 5 in items)
+            return not (-5 in items and 1 in items and 3 in items and 5 in items)
 
         self.assertEqual(
-            omegastar.run_bisect(command, list(range(-1_000_000, 1_000_000))), [1, 3, 5]
+            omegastar.run_bisect(command, list(range(-1_000_000, 1_000_000))),
+            [-5, 1, 3, 5],
         )
-        self.assertEqual(n_steps, 30)
+        self.assertEqual(n_steps, 69)
 
 
 if __name__ == "__main__":
